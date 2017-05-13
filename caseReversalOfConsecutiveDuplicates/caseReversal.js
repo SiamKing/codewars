c29e9f49a5932a7a3eafe641ef09fbf56c073fa9
SiamKing <thesiameseelectric@gmail.com>
2017-05-13 15:23:01 -0700

    refactored code

--- caseReversalOfConsecutiveDuplicates/caseReversal.js
+++ caseReversalOfConsecutiveDuplicates/caseReversal.js
@@ -1,22 +1,3 @@
 function reverseCase(str) {
-  let indices = [],
-      identical,
-      strArr = str.split(''),
-      i, j;
-
-  for (i = 0; i < str.length; i++) {
-    if (str[i] === str[i + 1] || str[i] === str[i - 1]) {
-      indices.push(i);
-    }
-  }
-
-  indices = [...new Set(indices)];
-
-  indices.forEach(index => {
-    identical = strArr.splice(index, 1).toString();
-    identical = identical.toUpperCase() === identical ? identical.toLowerCase() : identical.toUpperCase();
-    strArr.splice(index, 0, identical)
-  })
-
-  return strArr.join('');
+  return str.replace(/([a-zA-Z])\1+/g, char => char.toUpperCase() === char ? char.toLowerCase() : char.toUpperCase());
 }
