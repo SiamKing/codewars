d6cf091642990133944681e7ef2dadff1cbd7f42
SiamKing <thesiameseelectric@gmail.com>
2017-05-16 17:05:11 -0700

    refactored dna

--- complementaryDNA/dna.js
+++ complementaryDNA/dna.js
@@ -1,17 +1,5 @@
+const pairs = {'A':'T','T':'A','C':'G','G':'C'};
+
 function DNAStrand(dna){
-  var str = ''
-
-  dna.split('').forEach(char => {
-    if (char == 'A') {
-      str += 'T';
-    } else if (char == 'T') {
-      str += 'A';
-    } else if (char == 'C') {
-      str += 'G';
-    } else if (char == 'G') {
-      str += 'C';
-    }
-  })
-
-  return str;
+  return dna.split('').map(char => pairs[char]).join('')
 }
