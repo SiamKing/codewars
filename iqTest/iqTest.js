7cb3ee1c6e7e7349f89cb45d477ae70bb3d16253
SiamKing <thesiameseelectric@gmail.com>
2017-05-18 20:31:34 -0700

    finished twoOldestAges

--- iqTest/iqTest.js
+++ iqTest/iqTest.js
@@ -1,15 +1,7 @@
 function iqTest(numbers){
-  const evenArr = [],
-        oddArr = [],
-        numbersArr = numbers.split(' ');
-
-  numbersArr.forEach(num => {
-    if (num & 1) {
-      oddArr.push(num);
-    } else {
-      evenArr.push(num);
-    }
-  });
+  const numbersArr = numbers.split(' '),
+        evenArr = numbersArr.filter(num => !(num & 1)),
+        oddArr = numbersArr.filter(num => num & 1);
 
   return evenArr.length > oddArr.length ? numbersArr.indexOf(oddArr[0]) + 1 : numbersArr.indexOf(evenArr[0]) + 1;
 }
