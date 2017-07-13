9189bed1d39e8b53a56bd22c228a5daad08c4034
SiamKing <thesiameseelectric@gmail.com>
2017-07-12 19:38:49 -0700

    refactored areWeAlternate

--- areWeAlternate/alternate.js
+++ areWeAlternate/alternate.js
@@ -1,16 +1,3 @@
 function isAlt(word) {
-  const arr = ['a', 'e', 'i', 'o', 'u'];
-  let strArr = word.split(''),
-      isVowel = arr.includes(strArr[0]),
-      i = 1;
-
-  while (i < strArr.length) {
-    if (isVowel == arr.includes(strArr[i])) {
-      return false;
-    }
-    isVowel = arr.includes(strArr[i])
-    i++;
-  }
-
-  return true;
+  return !/[aeiou]{2}|[^aeiou]{2}/.test(word);
 }
