fa03fcbc19f892dd19d04367917e2de87cdb0a97
SiamKing <thesiameseelectric@gmail.com>
2017-07-13 19:13:46 -0700

    refactored freePizza

--- freePizza/freePizza.js
+++ freePizza/freePizza.js
@@ -1,17 +1,9 @@
 function pizzaRewards(customers, minOrders, minPrice) {
-  let currentCustomer,
-      rewardedCustomers =[],
-      i;
+  let rewardedCustomers =[];
 
   for (let key in customers) {
-    currentCustomer = [];
-    for (i = 0; i < customers[key].length; i++) {
-      if (customers[key][i] >= minPrice) {
-        currentCustomer.push(customers[key][i])
-      }
-    }
-    if (currentCustomer.length >= minOrders) {
-      rewardedCustomers.push(key);
+    if (customers[key].filter(price => price >= minPrice).length >= minOrders) {
+      rewardedCustomers.push(key)
     }
   }
   return rewardedCustomers;
