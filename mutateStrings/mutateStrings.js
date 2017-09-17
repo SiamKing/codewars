002dcb4b6b599780de95dd39ef71ab32c5d26098
SiamKing <thesiameseelectric@gmail.com>
2017-09-16 17:32:23 -0700

    added second solution for mutateStrings

--- mutateStrings/mutateStrings.js
+++ mutateStrings/mutateStrings.js
@@ -4,9 +4,25 @@ function mutateMyStrings(stringOne, stringTwo){
 
   for (let i = 0; i < stringOne.length; i++) {
     if (stringOne[i] !== stringTwo[i]) {
-      stringOne.splice(i, 1, stringTwo[i]);
+      stringOne[i] = stringTwo[i];
       str += '\n' + stringOne.join('');
     }
+  }
+  return str + '\n';
+}
+
+function mutateMyStrings(stringOne, stringTwo){
+  let str = stringOne,
+      i = 0,
+      arrayOne = stringOne.split('');
+
+  while (stringOne !== stringTwo) {
+    if (stringOne[i] !== stringTwo[i]) {
+      arrayOne[i] = stringTwo[i];
+      stringOne = arrayOne.join('');
+      str += '\n' + stringOne;
+    }
+    i++;
   }
   return str + '\n';
 }
